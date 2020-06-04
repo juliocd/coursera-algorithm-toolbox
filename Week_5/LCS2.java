6fd512f4d0320af65e1863cf587c5b029043e9fa
juliodiaz <julio@vimbly.com>
2020-06-03 23:01:48 -0500

    Update LCS2

--- Week_5/LCS2.java
+++ Week_5/LCS2.java
@@ -35,17 +35,12 @@ public class LCS2 {
               }
             }
           }
-          print2D(D);
         return D[s.length][t.length];
     }
 
     public static void print2D(int mat[][]) 
     { 
-        // Loop through all rows 
         for (int[] row : mat) 
-  
-            // converting each row as string 
-            // and then printing in a separate line 
             System.out.println(Arrays.toString(row)); 
     }
 
